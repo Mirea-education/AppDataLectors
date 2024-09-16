6e9370edf32cae3cbb9b0b06b4ce7c63c04d4929
Stas Makievsky <makievskij@mirea.ru>
2024-09-16 19:39:08 +0300

    first commit

---
+++ WpfTestApp/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfTestApp.DataClassApp;
+using WpfTestApp.PagesApp;
+using WpfTestApp.WindowsApp;
+
+namespace WpfTestApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //private WindowAbout okno = 
+        public MainWindow()
+        {
+            InitializeComponent();
+            ClassNavigation.frmContent = FrmMainContent;
+            FrmMainContent.Navigate(new PageMain());
+        }
+
+        private void BtnAlert_Click(object sender, RoutedEventArgs e)
+        {
+            new WindowAbout().Show();
+        }
+    }
+}
---
+++ WpfTestApp/PagesApp/PageMain.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfTestApp.DataClassApp;
+using WpfTestApp.WindowsApp;
+
+namespace WpfTestApp.PagesApp
+{
+    /// <summary>
+    /// Логика взаимодействия для PageMain.xaml
+    /// </summary>
+    public partial class PageMain : Page
+    {
+        public PageMain()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (TxbLogin.Text == "123" && TxbPass.Password == "123")
+                {
+                    ClassNavigation.frmContent.Navigate(new PageContent());
+                }
+                else {
+                    new WindowAllert().Show();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+    }
+}
